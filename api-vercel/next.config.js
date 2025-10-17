510fa2f1f4fecdc034ee44a3eb8fa7f89492895a
David Beamonde <179389440+Guayand0@users.noreply.github.com>
2025-10-18 00:44:46 +0200

    prueba 12.10 - api clash of clans

--- api-vercel/next.config.js
+++ api-vercel/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'js'],
-  experimental: {
-    appDir: false,
-  },
-  // 👇 aquí rediriges la carpeta "api-vercel/api" como "pages/api"
   webpack(config) {
     config.resolve.alias['@/api'] = './api-vercel/api'
     return config
